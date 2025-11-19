1f7672192e53c210caadb8c406cb49778dd9f075
Deepa <deepaykalar@gmail.com>
2025-11-20 00:56:44 +0530

    ui

--- backend/generate-swagger.js
+++ backend/generate-swagger.js
@@ -1,33 +1,43 @@
 import fs from "fs";
 import path from "path";
 import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUiDist from "swagger-ui-dist";
+
+const __dirname = path.resolve();
 
 const options = {
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "simple API",
+      title: "Simple API",
       version: "1.0.0",
-      description: "simple API with Swagger",
     },
-    servers: [{ url: "http://localhost:5000" }],
   },
-  apis: ["./server.js"],
+  apis: ["./backend/server.js"],
 };
 
-// Generate spec
 const swaggerSpec = swaggerJSDoc(options);
 
-// Ensure folder exists
-const outputDir = path.join(process.cwd(), "swagger-dist");
-if (!fs.existsSync(outputDir)) {
-  fs.mkdirSync(outputDir);
-}
+// create folder
+const outputDir = path.join(__dirname, "backend/swagger-dist");
+if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
 
-// Save swagger.json
-fs.writeFileSync(
-  path.join(outputDir, "swagger.json"),
-  JSON.stringify(swaggerSpec, null, 2)
-);
+// write swagger.json
+fs.writeFileSync(path.join(outputDir, "swagger.json"), JSON.stringify(swaggerSpec, null, 2));
 
-console.log("Swagger file generated at backend/swagger-dist/swagger.json");
+// copy swagger-ui-dist files
+const swaggerUiPath = swaggerUiDist.getAbsoluteFSPath();
+
+fs.readdirSync(swaggerUiPath).forEach(file => {
+  fs.copyFileSync(
+    path.join(swaggerUiPath, file),
+    path.join(outputDir, file)
+  );
+});
+
+// modify index.html
+let index = fs.readFileSync(path.join(outputDir, "index.html"), "utf8");
+index = index.replace("https://petstore.swagger.io/v2/swagger.json", "./swagger.json");
+fs.writeFileSync(path.join(outputDir, "index.html"), index);
+
+console.log("Swagger UI static site generated successfully!");
